4acc25bd711c3047c9eeb127c8c57b2ee8b16a4b
saif76r <apnar-github-email@example.com>
2026-08-16 00:10:12 +0600

    Initial project upload

---
+++ src/data/defaultProfile.ts
@@ -0,0 +1,34 @@
+import { ProfileData } from '../types';
+
+import logoImg from '../assets/images/bgi_logo_1786128343361.jpg';
+import profileImg from '../assets/images/sakif_asshead.jpg';
+
+export const BGI_LOGO_PATH = logoImg;
+
+export const DEFAULT_PROFILE: ProfileData = {
+    name: 'Abdullah Nur Sakif',
+
+    dept: ' Communication  ',
+
+    role: 'Assistant Director',
+
+    email: 'abdullahnursakif.bgicommunity@gmail.com ',
+
+    phone: '+880   01674791193 ',
+
+    bloodGroup: 'O+',
+
+    avatarUrl: profileImg,
+
+    bio: 'Passionate about building scalable web applications, open-source tech, and fostering student developer growth in BGI Community.',
+
+    batch: 'CSE 14th Batch',
+
+    location: 'Dhaka, Bangladesh',
+
+    socials: {
+        facebook: 'https://www.facebook.com/sakif.islam.560090',
+
+       
+    }
+};
